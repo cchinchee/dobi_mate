176003dff23ee7ce14b253b979cb68f554e93202
CCHINCHEE <CHONGCC90@GMAIL.COM>
2018-01-26 14:16:25 +0800

    create user table, bcrypt, welcome page

---
+++ db/migrate/20180126055458_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.json :users_avatar
+      t.string :confirmation_token, limit: 128
+      t.string :remember_token, limit: 128, null: false
+      t.integer :role, default: 0
+      t.string :password_digest
+      t.timestamps null: false
+    end
+
+    add_index :users, :email
+    add_index :users, :remember_token
+    
+  end
+end
+
